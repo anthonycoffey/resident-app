91d35855c3cf9265f156135608e51cd4f3e01866
Anthony Coffey <coffey.j.anthony@gmail.com>
2025-07-21 07:31:05 -0500

    shuffling config around to resolve build error

--- app.config.js
+++ app.config.js
@@ -2,6 +2,40 @@ module.exports = ({ config }) => {
   return {
     ...config,
     plugins: [
+      'expo-router',
+      [
+        'expo-image-picker',
+        {
+          photosPermission:
+            'The app accesses your photos to let you attach images to violation reports.',
+          cameraPermission:
+            'The app accesses your camera to let you take photos for violation reports.',
+        },
+      ],
+      [
+        '@sentry/react-native/expo',
+        {
+          url: 'https://sentry.io/',
+          project: 'amenilink-resident',
+          organization: '24hr-car-unlock-2f6982a53',
+        },
+      ],
+      [
+        '@react-native-firebase/app',
+        {
+          ios: {
+            useFrameworks: 'static',
+          },
+        },
+      ],
+      [
+        '@react-native-firebase/messaging',
+        {
+          ios: {
+            useFrameworks: 'static',
+          },
+        },
+      ],
       [
         'expo-notifications',
         {
